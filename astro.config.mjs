0d2ee7b58bd3db988383324303a9d664fd3cf788
PC-ANTONIO-CASA <antoniollorentepalomo@gmail.com>
2026-07-04 19:16:53 +0200

    desinstalado lo que sea de claudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   output: "static", 
   site: 'https://awhub.es',
   integrations: [sitemap()],
-  adapter: cloudflare({
-    // Desactivamos la detección automática de bindings de bases de datos e imágenes
-    imageService: 'passthrough', 
-  }), 
 });
