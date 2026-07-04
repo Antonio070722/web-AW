4ba89d0b48ac4042e76de4b66654742d1f0a7fa8
PC-ANTONIO-CASA <antoniollorentepalomo@gmail.com>
2026-07-04 18:53:07 +0200

    intentando solucionar error de compilación en cloudflare integrando adaptador cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import sitemap from "@astrojs/sitemap";
+import cloudflare from '@astrojs/cloudflare'; // 1. Importamos el nuevo adaptador
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
+  output: "static", // Mantenemos tu salida estática actual
   site: 'https://awhub.es',
   integrations: [sitemap()],
+  adapter: cloudflare(), // 2. Añadimos el adaptador de Cloudflare aquí
 });
